732a7aaa7f6d0cce4c3e23a21220068635033230
Sam Newby <sam.newby19@gmail.com>
2024-04-03 08:55:41 +0100

    chore: hardcode

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const express = require("express"),
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) =>
-  res.send(
-    `Dockerizing Node Application: ${(Math.random() + 1)
-      .toString(36)
-      .substring(7)}`
-  )
-);
+const rnd = (Math.random() + 1).toString(36).substring(7);
+
+app.get("/", (req, res) => res.send(`Dockerizing Node Application: ${rnd}`));
 
 app.listen(5000, () =>
   console.log(`⚡️[bootup]: Server is running at port: 5000`)
