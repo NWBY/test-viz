1b5b565a7e5404c3927f9f18177a809e98326f6c
Sam Newby <sam.newby19@gmail.com>
2024-05-10 23:37:52 +0100

    chore: alter1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const rnd = (Math.random() + 1).toString(36).substring(7);
 
 app.get("/", (req, res) => res.send(`Dockerizing Node Application: ${rnd}`));
-app.get("/yo", (req, res) => res.send("Yo yo yo"));
+app.get("/yo", (req, res) => res.send("Yo yo yo!"));
 
 app.listen(5000, () =>
   console.log(`⚡️[bootup]: Server is running at port: 5000`)
